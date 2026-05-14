81550dcbdb93b1aee4d9f15890ead88f504f5c75
Michael Cramer <michael@bigmichi1.de>
2026-05-14 10:57:30 +0200

    fix: deduplicate pending_codes before creating unique index in migration 0004
    Signed-off-by: Michael Cramer <michael@bigmichi1.de>

--- src/bot/database/migrations/0004_curious_living_lightning.sql
+++ src/bot/database/migrations/0004_curious_living_lightning.sql
@@ -1,1 +1,7 @@
+DELETE FROM `pending_codes`
+WHERE rowid NOT IN (
+    SELECT MIN(rowid)
+    FROM `pending_codes`
+    GROUP BY `code`
+);
 CREATE UNIQUE INDEX `pending_codes_code_unique` ON `pending_codes` (`code`);
